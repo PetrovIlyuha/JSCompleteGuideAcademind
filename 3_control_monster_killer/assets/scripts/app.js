ad138ba3206041a9eec69caa9e95d9348b62d12c
Ilya Petrov <dnamix1@gmail.com>
2019-11-03 12:47:36 +0300

    created a generic attack function using if statements

--- 3_control_monster_killer/assets/scripts/app.js
+++ 3_control_monster_killer/assets/scripts/app.js
@@ -8,8 +8,14 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-  const damage = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(mode) {
+  let maxDamage;
+  if (mode === "ATTACK") {
+    maxDamage = ATTACK_VALUE;
+  } else if ((mode = "STRONG_ATTACK")) {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  const damage = dealMonsterDamage(maxDamage);
   currentMonsterHealth -= damage;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
@@ -17,23 +23,18 @@ function attackHandler() {
     alert("Player Won!");
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
     alert("Monster Won!");
-  } else {
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
     alert("Draw...play again");
   }
 }
 
-function strongAttackHandler() {
-  const damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
-  currentMonsterHealth -= damage;
-  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-  currentPlayerHealth -= playerDamage;
-  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
-    alert("Player Won!");
-  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
-    alert("Monster Won!");
-  } else {
-    alert("Draw...play again");
-  }
+function attackHandler() {
+  attackMonster("ATTACK");
 }
+
+function strongAttackHandler() {
+  attackMonster("STRONG_ATTACK");
+}
+
 attackBtn.addEventListener("click", attackHandler);
 strongAttackBtn.addEventListener("click", strongAttackHandler);
